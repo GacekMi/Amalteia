95c0c920aa02f4e4e2cca7afa3bcfa28fdbee696
GacekMi <michal.gacek@software.dell.com>
2016-09-13 09:07:41 +0200

    Inicializace
    Adresarova struktura a demo projekty

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Amalteia</title>
+
+<style type="text/css">
+.myImage
+{
+    margin: auto;
+    display: block; 
+    margin-top: 50px;
+}
+
+.myText
+{
+    text-align:center;
+    margin-top: 50px;
+}
+.footer {
+   position:absolute;
+   bottom:0;
+   width:100%;
+   height:60px;   /* Height of the footer */
+   
+}
+.footertext
+{
+    text-align:center;
+}
+</style>
+
+</head>
+<body>
+    <img src="http://amalteia.cz/logo.png" alt="Amalteia" class="myImage"/>
+    <div class="myText">
+    <H1 style="color:#73001E" >Připravte se. Začínáme již 1.10.2016.</H1>
+    </div>
+<footer class="footer">
+   <p class="footertext" ><span> Copyright © 2016 All Rights Reserved. </span> <span> <a href="http://www.amalteia.cz/vypis_z_OR.pdf">Výpis z obchodního rejstříku.</a> </span></p>
+</footer>
+</body>
+</html>
