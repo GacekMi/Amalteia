b784bca3315a61f1f8bd0421606baea6e411fb63
GacekMi <michal.gacek@software.dell.com>
2016-09-15 09:33:15 +0200

    HTTPS
    Oprava htaccess na automaticke presmerovavani https.

--- index.php
+++ index.php
@@ -31,12 +31,12 @@
 
 </head>
 <body>
-    <img src="http://amalteia.cz/logo.png" alt="Amalteia" class="myImage"/>
+    <img src="https://amalteia.cz/logo.png" alt="Amalteia" class="myImage"/>
     <div class="myText">
     <H1 style="color:#73001E" >Připravte se. Začínáme již 1.10.2016.</H1>
     </div>
 <footer class="footer">
-   <p class="footertext" ><span> Copyright © 2016 All Rights Reserved. </span> <span> <a href="http://www.amalteia.cz/vypis_z_OR.pdf">Výpis z obchodního rejstříku.</a> </span></p>
+   <p class="footertext" ><span> Copyright © 2016 All Rights Reserved. </span> <span> <a href="https://www.amalteia.cz/vypis_z_OR.pdf">Výpis z obchodního rejstříku.</a> </span></p>
 </footer>
 </body>
 </html>
