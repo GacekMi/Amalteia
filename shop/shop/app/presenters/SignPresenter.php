2292588115e0d2621f67b72f292fc7bc315c49ec
GacekMi <michal.gacek@software.dell.com>
2016-10-09 10:25:56 +0200

    Uprava formularu a paticky at je plovouci

--- shop/shop/app/presenters/SignPresenter.php
+++ shop/shop/app/presenters/SignPresenter.php
@@ -45,7 +45,7 @@ class SignPresenter extends BasePresenter
         $form->addCheckbox('remember', $this->translator->translate("ui.signMessage.remeberLogin"));
 
         $form->addSubmit('send', $this->translator->translate("ui.signMessage.loginButton"));
-      //  $form->addReCaptcha('captcha', NULL, $this->translator->translate("ui.signMessage.reCaptchaMessage"));
+        $form->addReCaptcha('captcha', NULL, $this->translator->translate("ui.signMessage.reCaptchaMessage"));
         $form['send']->getControlPrototype()->class('btn btn-success');
         $renderer = $form->getRenderer();
         $renderer->wrappers['controls']['container'] = 'div';
